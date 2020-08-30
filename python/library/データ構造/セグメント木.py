c68d6b9b86e15bc57642d777dd32821a6965352e
masa-aa <masayayamaguchi480@gmail.com>
2020-08-31 02:40:48 +0900

    Update セグメント木.py

--- python/library/データ構造/セグメント木.py
+++ python/library/データ構造/セグメント木.py
@@ -1,23 +1,24 @@
 # Segment Tree
 class Segment:
     def __init__(self, N, init_val):
-        self.N0 = 2**(N-1).bit_length()
+        self.N0 = 2 ** (N - 1).bit_length()
         # 0-indexedで管理
-        self.dat = [identity_element]*(2*self.N0)
+        self.dat = [identity_element] * (2 * self.N0)
+
         # 値を代入
         for i in range(N):
-            self.dat[i+self.N0-1]=init_val[i]    
+            self.dat[i + self.N0 - 1] = init_val[i]
         # 構築
-        for i in range(self.N0-2,-1,-1) :
-            self.dat[i]=segfunc(self.dat[2*i+1],self.dat[2*i+2]) 
+        for i in range(self.N0 - 2, -1, -1):
+            self.dat[i] = segfunc(self.dat[2 * i + 1], self.dat[2 * i + 2])
 
     # k番目の要素の値をxに変更
     def update(self, k, x):
-        k += self.N0-1
+        k += self.N0 - 1
         self.dat[k] = x
         while k >= 0:
-            k = (k-1)//2
-            self.dat[k] = segfunc(self.dat[2*k+1], self.dat[2*k+2])
+            k = (k - 1) // 2
+            self.dat[k] = segfunc(self.dat[2 * k + 1], self.dat[2 * k + 2])
 
     # 区間[l,r)の最小値を求める
     def query(self, l, r):
@@ -28,9 +29,9 @@ class Segment:
         while L < R:
             if R & 1:
                 R -= 1
-                s = segfunc(s, self.dat[R-1])
+                s = segfunc(s, self.dat[R - 1])
             if L & 1:
-                s = segfunc(s, self.dat[L-1])
+                s = segfunc(s, self.dat[L - 1])
                 L += 1
             L >>= 1
             R >>= 1
@@ -39,6 +40,9 @@ class Segment:
 # segfunc : min, +, *, xor, gcd など
 # identity_element : 単位元(min:inf, 和:0, 積:1, xor:0, gcd:0)
 
-def segfunc(x,y):
-    return min(x,y) # 変える
-identity_element = 2**31-1 # 変える
+
+def segfunc(x, y):
+    return min(x, y)  # 変える
+
+
+identity_element = 2**31 - 1  # 変える
