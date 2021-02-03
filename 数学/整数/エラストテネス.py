5509107c323c030504334b15196806cb324e50d0
masa-aa <60830652+masa-aa@users.noreply.github.com>
2021-02-03 19:00:51 +0900

    update

--- 数学/整数/エラストテネス.py
+++ 数学/整数/エラストテネス.py
@@ -4,14 +4,15 @@ from math import sqrt
 def Eratosthenes(n):
     """n未満の素数列挙 """
     if n < 4:
-        return [] if n < 2 else [2]
+        return [] if n <= 2 else [2]
     n, correction = n - n % 6 + 6, 2 - (n % 6 > 1)
     sieve = [True] * (n // 3)
     for i in range(1, int(sqrt(n)) // 3 + 1):
         if sieve[i]:
             k = 3 * i + 1 | 1
             sieve[k * k // 3::2 * k] = [False] * ((n // 6 - k * k // 6 - 1) // k + 1)
-            sieve[k * (k - 2 * (i & 1) + 4) // 3::2 * k] = [False] * ((n // 6 - k * (k - 2 * (i & 1) + 4) // 6 - 1) // k + 1)
+            sieve[k * (k - 2 * (i & 1) + 4) // 3::2 * k] = [False] * \
+                ((n // 6 - k * (k - 2 * (i & 1) + 4) // 6 - 1) // k + 1)
 
     return [2, 3] + [3 * i + 1 | 1 for i in range(1, n // 3 - correction) if sieve[i]]
 
