a83b60a0454d66a581bffd2bc572513b46530e72
masa-aa <masayayamaguchi480@gmail.com>
2021-02-05 03:08:06 +0900

    Update LCA.py

--- グラフ/LCA.py
+++ グラフ/LCA.py
@@ -1,3 +1,4 @@
+# issue dfsが再帰
 import sys
 sys.setrecursionlimit(10000000)
 input = sys.stdin.readline
@@ -14,40 +15,29 @@ class LowestCommonAncestor:
         self._rmq_init(self.depth_list)
 
     def _rmq_init(self, arr):
-        n = len(arr)
-        self.N0 = 1 << (n - 1).bit_length()
-        self.dat = [self.n] * (self.N0 - 1) + arr + [self.n] * (self.N0 - n + 1)
-        self.index = [0] * (self.N0 - 1) + list(range(n)) + [0] * (self.N0 - n + 1)
-        dat = self.dat
-        index = self.index
-        for i in range(self.N0 - 2, -1, -1):
-            if dat[2 * i + 1] > dat[2 * i + 2]:
-                dat[i] = dat[2 * i + 2]
-                index[i] = index[2 * i + 2]
-            else:
-                dat[i] = dat[2 * i + 1]
-                index[i] = index[2 * i + 1]
+        n = self.mod = len(arr)
+        self.seg_len = 1 << (n - 1).bit_length()
+        self.seg = [self.n * n] * (2 * self.seg_len)
+        seg = self.seg
+        for i, e in enumerate(arr):
+            seg[self.seg_len + i] = n * e + i
+        for i in range(self.seg_len - 1, 0, -1):
+            seg[i] = min(seg[2 * i], seg[2 * i + 1])
 
     def _rmq_query(self, l, r):
         """最小値となるindexを返す"""
-        l += self.N0; r += self.N0
-        s = self.n
-        dat = self.dat
-        index = self.index
+        l += self.seg_len; r += self.seg_len
+        res = self.n * self.mod
+        seg = self.seg
         while l < r:
             if r & 1:
                 r -= 1
-                if s > dat[r - 1]:
-                    s = dat[r - 1]
-                    res = index[r - 1]
+                res = min(res, seg[r])
             if l & 1:
-                if s > dat[l - 1]:
-                    s = dat[l - 1]
-                    res = index[l - 1]
+                res = min(res, seg[l])
                 l += 1
-            l >>= 1
-            r >>= 1
-        return res
+            l >>= 1; r >>= 1
+        return res % self.mod
 
     def _dfs(self, G, vertex, parent, depth):
         self.id[vertex] = self.visit_id
