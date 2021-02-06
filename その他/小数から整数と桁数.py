664c17c183b6a41c226cb10000ae49bace51075b
masa-aa <masayayamaguchi480@gmail.com>
2021-02-07 00:26:24 +0900

    Update 小数から整数と桁数.py

--- その他/小数から整数と桁数.py
+++ その他/小数から整数と桁数.py
@@ -1,4 +1,6 @@
 # str型の小数(31.415 etc)が与えられて(31415,3)みたいなのを返す.
 
 def float2int(FLOAT):
+    if "." not in FLOAT:
+        return int(FLOAT), 0
     return int(FLOAT.replace(".", "")), len(FLOAT) - FLOAT.index(".") - 1
