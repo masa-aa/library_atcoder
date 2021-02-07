576f59229089b7bd270ceec599596e3bebb453ba
masa-aa <masayayamaguchi480@gmail.com>
2021-02-08 03:53:19 +0900

    昔のものを修正

--- 数学/各桁の和.py
+++ 数学/各桁の和.py
@@ -1,14 +1,10 @@
-"""
-各桁の和を求める
-"""
-
-
 def dgsm(n):
+    """各桁の和を求める"""
     ans = 0
     while n > 0:
         ans += n % 10
         n //= 10
-    return(ans)
+    return ans
 
 # print(dgsm(1342))
 # 1+3+4+2=10
--- 数学/多倍長の余り.py
+++ 数学/多倍長の余り.py
@@ -1,15 +1,8 @@
-# n%a を求める
-
-n = list(map(int, input()))
-
-
-def rem(n, a):
+def rem(n: list, a: int):
+    """ n % a を求める"""
     ans = 0
     for i in n:
         ans *= 10
         ans += i
         ans %= a
     return ans
-
-
-# print(rem(int(input())),n)
