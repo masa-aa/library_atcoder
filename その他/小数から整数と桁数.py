615328f4b1af712bc4f4d5df2e8d784737130297
masa-aa <masayayamaguchi480@gmail.com>
2021-02-07 20:24:29 +0900

    Update 小数から整数と桁数.py

--- その他/小数から整数と桁数.py
+++ その他/小数から整数と桁数.py
@@ -1,6 +1,5 @@
-# str型の小数(31.415 etc)が与えられて(31415,3)みたいなのを返す.
-
-def float2int(FLOAT):
+def float2int(FLOAT: str) -> tuple:
+    """ str型の小数("31.415" etc)が与えられて(31415,3)みたいなのを返す. """
     if "." not in FLOAT:
         return int(FLOAT), 0
     return int(FLOAT.replace(".", "")), len(FLOAT) - FLOAT.index(".") - 1
