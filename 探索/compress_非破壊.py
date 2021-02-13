4c367aebc794389571d0ec8523afd34917fdd83e
masa-aa <masayayamaguchi480@gmail.com>
2021-02-14 01:38:31 +0900

    Update compress_非破壊.py

--- 探索/compress_非破壊.py
+++ 探索/compress_非破壊.py
@@ -1,3 +1,5 @@
 def compress(arr):
-    """座圧の対応表を返す {a:b,c:d,...} a -> b, c -> d, ..."""
-    return {e: i for i, e in enumerate(sorted(set(arr)))}
+    """座圧の対応表と逆対応表を返す {a:b,c:d,...} a -> b, c -> d, ..."""
+    com = {e: i for i, e in enumerate(sorted(set(arr)))}
+    rcom = list(com.keys())
+    return com, rcom
