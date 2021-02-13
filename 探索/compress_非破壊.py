7488eea00de4b50e95525119e4e9dc1fcae54186
masa-aa <masayayamaguchi480@gmail.com>
2021-02-14 01:19:27 +0900

    Update compress_非破壊.py

--- 探索/compress_非破壊.py
+++ 探索/compress_非破壊.py
@@ -1,5 +1,3 @@
 def compress(arr):
-    """座圧の対応表を返す {a:b,c:d,...}a→b, c→d,..."""
-    *XS, = set(arr)
-    XS.sort()
-    return {e: i for i, e in enumerate(XS)}
+    """座圧の対応表を返す {a:b,c:d,...} a -> b, c -> d, ..."""
+    return {e: i for i, e in enumerate(sorted(set(arr)))}
