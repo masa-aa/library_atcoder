f6676cecf99640e2fe9afa81ebdfd838e0d68c4b
masa-aa <60830652+masa-aa@users.noreply.github.com>
2021-02-17 19:16:44 +0900

    Update LowLink.py

--- グラフ/LowLink.py
+++ グラフ/LowLink.py
@@ -1,28 +1,38 @@
+# 未完成
 class LowLink:
     def __init__(self, G, root=0):
         self.n = n = len(G)
+        self.root = root
         self.order = [-1] * n
         self.lowlink = [0] * n
-        self.dfs(G, root)
+        self.parents = [-1] * n
+        self.G = G
+        self.dfs(root)
 
-    def dfs(self, G, root):
+    def dfs(self, root):
         """ 非再帰で深さ優先探索を行う """
+        G = self.G
         order = self.order
         lowlink = self.lowlink
         use = [0] * self.n
         v = root
         it = [0] * self.n
-        parents = [-1] * self.n
+        parents = self.parents
         num = 0
-        while v != -1:
-            print(v)
+        while True:
             use[v] = 1
-            order[v] = lowlink[v] = num
-            num += 1
+            if order[v] == -1:
+                order[v] = lowlink[v] = num
+                num += 1
             g = G[v]
             while it[v] < len(g) and use[g[it[v]]]:
+                if parents[v] != g[it[v]]:
+                    lowlink[v] = min(order[g[it[v]]], lowlink[v])
                 it[v] += 1
             if it[v] == len(g):
+                if v == root:
+                    break
+                lowlink[parents[v]] = min(lowlink[parents[v]], lowlink[v])
                 v = parents[v]
             else:
                 child = g[it[v]]
@@ -30,6 +40,49 @@ class LowLink:
                 it[v] += 1
                 v = child
 
+    def bridge(self):
+        G = self.G
+        order = self.order
+        lowlink = self.lowlink
+        res = []
+        for u, g in enumerate(G):
+            for v in g:
+                if order[u] < lowlink[v]:
+                    res.append((u, v))
+        return res
 
-es = [[1, 4], [0, 2, 3], [1], [1], [0, 5], [4]]
-LowLink(es)
+    def articulation_point(self):
+        G = self.G
+        root = self.root
+        order = self.order
+        lowlink = self.lowlink
+        parents = self.parents
+        res = set()
+        for u, g in enumerate(G):
+            if u == root:
+                if parents.count(u) >= 2:
+                    res.add(u)
+                continue
+            for v in g:
+                if parents[u] == v:
+                    continue
+                if order[u] <= lowlink[v]:
+                    res.add(u)
+        return res
+
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+es = [[] for _ in range(n)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    es[x].append(y)
+    es[y].append(x)
+
+res = LowLink(es).articulation_point()
+res = sorted(res)
+if res:
+    print(*res, sep="\n")
+print(LowLink(es).bridge())
