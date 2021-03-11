7c2deafb27e4f1e0f281c3d2bb6f29c3e7ddd318
masa-aa <60830652+masa-aa@users.noreply.github.com>
2021-03-11 16:57:14 +0900

    Update Miller-Rabin.py

--- 数学/整数/Miller-Rabin.py
+++ 数学/整数/Miller-Rabin.py
@@ -1,33 +1,36 @@
-def Miller(N):
-    """素数判定, 決定的アルゴリズム"""
-    v = [2, 7, 61] if N < 4_759_123_141 else \
-        [2, 3, 5, 7, 11, 13, 17] if N < 341_550_071_728_321 else \
-        [2, 3, 5, 7, 11, 13, 17, 19, 23] if N < 3_825_123_056_546_413_051 else \
-        [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
-    if N < 2:
+def miller_rabin(n):
+    """素数判定 log(n)"""
+    if n < 2:
         return False
-    if N in v:
+
+    base = [2, 7, 61] if n < 4_759_123_141 else \
+           [2, 3, 5, 7, 11, 13, 17] if n < 341_550_071_728_321 else \
+           [2, 3, 5, 7, 11, 13, 17, 19, 23] if n < 3_825_123_056_546_413_051 else \
+           [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+
+    if n in base:
         return True
-    d = N - 1
-    s = 0
+
+    if n % 2 == 0:
+        return False
+
+    d = n - 1
     while d % 2 == 0:
         d //= 2
-        s += 1
 
-    for a in v:
-        if pow(a, d, N) != 1:
-            ok = True
-            for r in range(s):
-                if pow(a, d * 1 << r, N) == N - 1:
-                    ok = 0
-                    break
-            if ok:
-                return False
+    for a in base:
+        t = d
+        y = pow(a, t, n)
+        while t != n - 1 and y != 1 and y != n - 1:
+            y = y * y % n
+            t *= 2
+        if y != n - 1 and t % 2 == 0:
+            return False
     return True
 
 
 if __name__ == "__main__":
-    print(Miller(10**30 + 57))
+    print(miller_rabin(2357))
 
 # v = [2, 3, 5, 7, 325, 9375, 28178, 450775, 9780504, 1795265022]
 # verify
