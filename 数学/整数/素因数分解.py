984c772fe60c3a498deb7328d065db0265164a33
masa-aa <masayayamaguchi480@gmail.com>
2021-03-27 01:30:57 +0900

    Update 素因数分解.py

--- 数学/整数/素因数分解.py
+++ 数学/整数/素因数分解.py
@@ -4,24 +4,17 @@ def factorization(n: int) -> list:
     if n == 1:
         return []
     arr = []
-    temp = n
-    if temp % 2 == 0:
-        cnt = 0
-        while temp % 2 == 0:
-            cnt += 1
-            temp //= 2
-            arr.append(2)
+    while n % 2 == 0:
+        n //= 2
+        arr.append(2)
     for i in range(3, n + 1, 2):
         if i * i > n:
             break
-        if temp % i == 0:
-            cnt = 0
-            while temp % i == 0:
-                cnt += 1
-                temp //= i
-                arr.append(i)
+        while n % i == 0:
+            n //= i
+            arr.append(i)
 
-    if temp != 1:
-        arr.append(temp)
+    if n != 1:
+        arr.append(n)
 
     return arr
