3defdd719d602f50d34db2fa32ad852edad2e0f6
Jessica-nao <74441749+Jessica-nao@users.noreply.github.com>
2021-05-30 13:15:13 +0900

    2直線の交点、外心を追加
    貼るところ間違えていたのと、
    法線を垂直二等分線により正確に修正しました。

--- 幾何/幾何2D.py
+++ 幾何/幾何2D.py
@@ -114,3 +114,85 @@ def is_cross(a, b, c, d):
     if s * t > 0:
         return False
     return True
+
+
+def mid_point(p1, p2):
+    '''
+    中点を返す。
+    '''
+    x1, y1 = p1
+    x2, y2 = p2
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+    return (x,y)
+
+
+def is_collinear(p1,p2,p3):
+    '''
+    3点が同一直線上にあるかどうか。
+    '''
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+    return (x1 - x2) * (y3 - y2) == (y1 - y2) * (x3 - x2)
+
+
+def is_parallel(v1, v2):
+    '''
+    2つのベクトルが直線が平行かどうか。
+    '''
+    x1, y1 = v1
+    x2, y2 = v2
+    return x1 * y2 == y1 * x2
+
+
+def get_vector(p1, p2):
+    '''
+    2点 p1, p2 に対し、ベクトル p2p1 を返す。
+    '''
+    vx = p2[0] - p1[0]
+    vy = p2[1] - p1[1]
+    return (vx, vy)
+
+
+def get_intersection(l1, l2):
+    '''
+    2直線、l1, l2 の交点を返す。
+    交点がない場合（共有点が無数にある場合も含む）は None を返す。
+    したがって、共有点を求めたい場合は別に処理が必要。
+    l1 = (p1, p2) で、2点 p1, p2 を通る直線。
+    '''
+    v1 = get_vector(*l1)
+    v2 = get_vector(*l2)
+    if is_parallel(v1, v2):
+        return None
+    a, b = l1[0]
+    u, v = get_vector(*l1)
+    c, d = l2[0]
+    w, x = get_vector(*l2)
+
+    s = ((a-c)*v - (b-d)*u) / (w*v - x*u)
+    return (c+s*w, d+s*x)
+
+def get_perpendicular_bisector(p1,p2):
+    '''
+    p1, p2 を両端とする線分の、垂直二等分線を返す。
+    '''
+    if p1 == p2:
+        return None
+    m = mid_point(p1, p2)
+    a, b = get_vector(p1, p2)
+    v = (-b, a)
+    p = (m[0]+v[0], m[1]+v[1])
+    return (m, p)
+
+
+def get_circumcenter(p1,p2,p3):
+    '''
+    外心が存在すれば返す。
+    '''
+    if is_collinear(p1,p2,p3):
+        return None
+    l1 = get_perpendicular_bisector(p1, p2)
+    l2 = get_perpendicular_bisector(p2, p3)
+    return get_intersection(l1, l2)
