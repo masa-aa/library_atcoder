13fb583de0a5f0909aa00399811b93e6e6ff3ce8
masa-aa <masayayamaguchi480@gmail.com>
2021-06-26 03:44:59 +0900

    Update 2部グラフ判定.py

--- グラフ/2部グラフ判定.py
+++ グラフ/2部グラフ判定.py
@@ -1,38 +1,82 @@
-"""
-頂点数nと隣接リストesが与えられて, そのグラフが2部グラフか判定する
-"""
-import sys
-sys.setrecursionlimit(100000000)
-color = [0 for i in range(n)]
+class UnionFind:
+    __slots__ = ["N", "root"]
+
+    def __init__(self, N):
+        """
+        N:要素数
+        root:各要素の親要素の番号を格納するリスト.
+             ただし, root[x] < 0 ならその頂点が根で-root[x]が木の要素数.
+        """
+        self.N = N
+        self.root = [-1] * N
+
+    def __repr__(self):
+        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
+
+    def find(self, x):
+        """頂点xの根を見つける"""
+        r = x
+        while self.root[r] >= 0:
+            r = self.root[r]
+
+        while self.root[x] >= 0:
+            self.root[x], x = r, self.root[x]
+
+        return r
+
+    def union(self, x, y):
+        """x, yが属する木をunion"""
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+        elif self.root[y] < self.root[x]:
+            x, y = y, x
+        self.root[x] += self.root[y]
+        self.root[y] = x
+
+    def same(self, x, y):
+        """xとyが同じグループに属するかどうか"""
+        return self.find(x) == self.find(y)
+
+    def count(self, x):
+        """頂点xが属する木のサイズを返す"""
+        return - self.root[self.find(x)]
+
+    def members(self, x):
+        """xが属する木の要素を列挙"""
+        _root = self.find(x)
+        return [i for i in range(self.N) if self.find(i) == _root]
+
+    def roots(self):
+        """森の根を列挙"""
+        return [i for i, x in enumerate(self.root) if x < 0]
+
+    def group_count(self):
+        """連結成分の数"""
+        return len(self.roots())
+
+    def all_group_members(self):
+        """{ルート要素: [そのグループに含まれる要素のリスト], ...}の辞書を返す"""
+        groups = {r: [] for r in self.roots()}
+        for i in range(self.N):
+            groups[self.find(i)].append(i)
+        return groups
 
 
-def dfs(v, c):
-    color[v] = c
-    for i in es[v]:
-        if color[i] == c:
-            return False
-        if color[i] == 0 and not dfs(i, -c):
-            return False
-    return True
-# グラフが連結の場合
-# def is_bipartite():
-#     return dfs(0,1)
+class isBipartiteGraph:
+    """classにする必要はなさそうだけど覚書として"""
 
-# グラフが非連結の場合
-# def is_bipartite():
-#     for i in range(n):
-#         if color[i]==0:
-#             if not dfs(i, 1):
-#                 return False
-#     return True
+    def __init__(self, n: int):
+        self.n = n
+        self.uf = UnionFind(2 * n)
 
-# example
-# n = 5 #連結の場合を使う
-# es = [[1,2,3],[0,2],[0,1],[0,4],[3]]
-# color = [0 for i in range(n)]
-# is_bipartite() #Fasle
+    def add_edge(self, u: int, v: int):
+        self.uf.union(u, self.n + v)
+        self.uf.union(v, self.n + u)
 
-# n = 7 #非連結の場合を使う
-# es = [[1,3],[0,2],[1,3],[0,2,4],[3],[6],[5]] # True
-# color = [0 for i in range(n)]
-# print(is_bipartite())
+    def is_bipartite_graph(self):
+        for i in range(self.n):
+            if self.uf.same(i, i + self.n):
+                return False
+        return True
