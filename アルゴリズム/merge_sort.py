9fc70cc517112fe4eb0d8a0a47af27f4668d0eec
masa-aa <masayayamaguchi480@gmail.com>
2021-08-20 23:08:56 +0900

    Update merge_sort.py

--- アルゴリズム/merge_sort.py
+++ アルゴリズム/merge_sort.py
@@ -22,13 +22,37 @@ def merge_sort(a: list) -> None:
     while m < n:
         m2 = m + m
         for i in range(n // m2):
-            merge_array = merge(a[i * m2:i * m2 + m], a[i * m2 + m:(i + 1) * m2])
+            # merge_array = merge(a[i * m2:i * m2 + m], a[i * m2 + m:(i + 1) * m2])
+            s = a[i * m2:i * m2 + m]
+            t = a[i * m2 + m:(i + 1) * m2]
             k = i * m2
-            for j, e in enumerate(merge_array):
-                a[k + j] = e
+            s_idx = t_idx = 0
+            for k in range(i * m2, (i + 1) * m2):
+                if t_idx == m:
+                    a[k] = s[s_idx]
+                    s_idx += 1
+                elif t_idx == m or t[t_idx] < s[s_idx]:
+                    a[k] = t[t_idx]
+                    t_idx += 1
+                else:
+                    a[k] = s[s_idx]
+                    s_idx += 1
+
         k = n // m2 * m2
         if n - k > m:
-            merge_array = merge(a[k:k + m], a[k + m:])
-            for j, e in enumerate(merge_array):
-                a[k + j] = e
+            # merge_array = merge(a[k:k + m], a[k + m:])
+            s = a[k:k + m]
+            t = a[k + m:]
+            t_size = n - k - m
+            s_idx = t_idx = 0
+            for k in range(k, n):
+                if t_idx == t_size:
+                    a[k] = s[s_idx]
+                    s_idx += 1
+                elif t_idx == m or t[t_idx] < s[s_idx]:
+                    a[k] = t[t_idx]
+                    t_idx += 1
+                else:
+                    a[k] = s[s_idx]
+                    s_idx += 1
         m = m2
