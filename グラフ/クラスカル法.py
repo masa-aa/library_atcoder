ff501383db7d2fbf5ec433349563e8d4da03a42f
masa-aa <masayayamaguchi480@gmail.com>
2021-09-12 18:35:32 +0900

    Update クラスカル法.py

--- グラフ/クラスカル法.py
+++ グラフ/クラスカル法.py
@@ -1,11 +1,10 @@
-def krascal(n: "頂点数", D: "隣接リスト"):
+def krascal(n: "頂点数", D: "隣接リスト") -> int:
     """D[i] = (x, y, (x, y)間のcost), 最小全域木のコストを返す"""
     uf = UnionFind(n)
     D.sort(key=lambda x: x[2])
     cost = 0
     for x, y, c in D:
-        if not uf.same(x, y):
-            uf.union(x, y)
+        if not uf.union(x, y):
             cost += c
     return cost
 
@@ -13,26 +12,25 @@ def krascal(n: "頂点数", D: "隣接リスト"):
 class UnionFind:
     __slots__ = ["root"]
 
-    def __init__(self, N):
+    def __init__(self, N: int) -> None:
         self.root = [-1] * N
 
-    def find(self, x):
+    def find(self, x: int) -> int:
         while self.root[x] >= 0:
             x = self.root[x]
         return x
 
-    def union(self, x, y):
+    def union(self, x: int, y: int) -> bool:
+        """返り値は同じグループか否か"""
         x = self.find(x)
         y = self.find(y)
         if x == y:
-            return
+            return True
         elif self.root[y] < self.root[x]:
             x, y = y, x
         self.root[x] += self.root[y]
         self.root[y] = x
-
-    def same(self, x, y):
-        return self.find(x) == self.find(y)
+        return False
 
 
 # if __name__ == '__main__':
