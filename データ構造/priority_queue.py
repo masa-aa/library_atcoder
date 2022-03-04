7621ce2f86ab8ea3a5739defa5196b691382fc8d
masa-aa <masayayamaguchi480@gmail.com>
2022-03-04 16:25:34 +0900

    Update priority_queue.py

--- データ構造/priority_queue.py
+++ データ構造/priority_queue.py
@@ -16,7 +16,8 @@ def heappop(a: list):
     res = a.pop()
     n = len(a)
     m = 0
-    while True:
+    limit = n // 2
+    while m < limit:
         m2 = m + m + 1
         if m2 + 1 < n:
             if a[m2] < a[m2 + 1]:
@@ -25,62 +26,17 @@ def heappop(a: list):
                     m = m2
                 else:
                     break
-            else:
                 if a[m] > a[m2 + 1]:
                     a[m], a[m2 + 1] = a[m2 + 1], a[m]
                     m = m2 + 1
                 else:
                     break
 
-        elif m2 < n:
+        else:
             if a[m] > a[m2]:
                 a[m], a[m2] = a[m2], a[m]
                 m = m2
             else:
                 break
 
-        else:
-            break
-
     return res
-
-
-def test_push():
-    a = []
-    for i in rand:
-        heappush(a, i)
-    return a
-
-
-def test_pop(a):
-    k = 0
-    while a:
-        k += heappop(a)
-    return k
-
-
-n = 10**6
-rand = [randrange(0, 10 ** 9) for _ in range(n)]
-
-start = time()
-a = test_push()
-end = time()
-print(f"my_push, {end-start} sec")
-
-start = time()
-test_pop(a)
-end = time()
-
-print(f"my_pop, {end-start} sec")
-
-from heapq import heappush, heappop
-
-start = time()
-a = test_push()
-end = time()
-print(f"python_push, {end-start} sec")
-
-start = time()
-test_pop(a)
-end = time()
-print(f"python_pop, {end-start} sec")
